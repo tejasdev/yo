f7388ded5809470ee9721e3194b77eb15b2fd4f3
Tejas <tejasdev1995@gmail.com>
2016-10-25 23:55:48 +0530

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -492,10 +492,11 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
+	var top = document.body.scrollTop / 1250;
 
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((top) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
